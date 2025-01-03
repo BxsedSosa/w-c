abc0f929f1e539ad9a7a4be05322edb6d5891cbd
BxsedSosa <donovanxabad@gmail.com>
2025-01-03 06:51:18 -0800

    fixed descriptions

--- fundamentals/t-f.js
+++ fundamentals/t-f.js
@@ -1,4 +1,4 @@
-/* Summary: Check if you are able to watch tv today */
+/* Summary: Check whether the current day is either a holiday OR is NOT the weekday if so, return true. Otherwise, return false */
 function canWatchTv(isHoliday, isWeekday) {
   // Check if the day is a `Holiday` OR `is NOT the weekday`, return true.
   if (isHoliday || isWeekday === false) {
@@ -9,7 +9,7 @@ function canWatchTv(isHoliday, isWeekday) {
   }
 }
 
-/* Summary: Check do both business partners agree to with each other */
+/* Summary: Compare that both business partners have the same idea if so, return true. Otherwise, return false */
 function doTheyAgree(partner1Decision, partner2Decision) {
   // Check if both partners are on the same page, if so return true.
   if (partner1Decision === partner2Decision) {
@@ -20,9 +20,9 @@ function doTheyAgree(partner1Decision, partner2Decision) {
   }
 }
 
-/* Summary: Check to see if the store is open or closed */
+/* Summary: If the month is currently "july" OR the weekday is "monday" the store would be closed, return false. Otherwise, return true */
 function isOpen(weekday, month) {
-  // Check if the  check if the `month` is "july" OR `weekday` is "monday", return false.
+  // Check if the `month` is "july" OR `weekday` is "monday", return false.
   if (month === "july" || weekday === "monday") {
     return false;
     // Otherwise: The store would be open, return true.
